249f6c7805eea9a9be00e52b62b75cb8ffd436a3
Keriann <keri.gannon@pm.me>
2024-04-11 16:56:27 -0500

    add test for post comment

--- tests/api.test.js
+++ tests/api.test.js
@@ -28,4 +28,30 @@ describe('Api', () => {
       expect(result).toEqual(mockComments);
     });
   });
+
+  describe('createComment', () => {
+    it('creates a comment', async () => {
+      const mockResponse = { success: true };
+
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue(mockResponse),
+      });
+
+      const url = 'https://localhost:3001/createComment';
+      const method = 'post';
+      const comment = { name: 'Keri', message: 'Hello!' };
+
+      const result = await Api.post(url, comment);
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        method,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(comment),
+      });
+      expect(result).toEqual(mockResponse);
+    });
+  });
 });
